114b85fed2a32f67839070a01f39bc2ac72468d8
Jacob Murray <96128065+jacob-murray@users.noreply.github.com>
2026-07-11 16:22:40 -0400

    adding import and alias sample

---
+++ python/import_namespace.py
@@ -0,0 +1,32 @@
+"""
+code sample demonstrating import, from {package} import, and aliasing
+"""
+
+# importing sqrt from the math module
+from math import sqrt
+
+# let's check the module!
+sqrt.__module__
+
+from cmath import sqrt
+
+# let's check the module again!
+sqrt.__module__
+
+# as we can see, functions/methods are simply objects and sequence matters
+
+# alias?
+from cmath import sqrt as complex_sqrt
+from math import sqrt
+
+complex_sqrt.__module__
+sqrt.__module__
+
+# we could also simply alias the package
+# import entire package, then namespace
+import math
+import cmath
+
+math.sqrt.__module__
+cmath.sqrt.__module__
+
